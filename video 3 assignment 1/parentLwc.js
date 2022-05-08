267534c1a14654bf7c179e46a6c9f70b258b12d3
rishsingh1999 <104893374+rishsingh1999@users.noreply.github.com>
2022-05-09 00:20:08 +0530

    Add files via upload

---
+++ video 3 assignment 1/parentLwc.js
@@ -0,0 +1,11 @@
+import { LightningElement, track } from "lwc";
+
+export default class ParentLwc extends LightningElement {
+    @track progressValue = 0;
+    
+    hanldeProgressValueChange(event) {
+      
+        this.progressValue = event.detail;
+    
+  }
+}
