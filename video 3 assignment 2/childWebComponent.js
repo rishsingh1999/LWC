c7717f47d18341ecae79c64aa41a2d56254c89d9
rishsingh1999 <104893374+rishsingh1999@users.noreply.github.com>
2022-05-15 19:49:53 +0530

    Delete childWebComponent.js

--- video 3 assignment 2/childWebComponent.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, track,api } from "lwc";
-
-export default class ChildWebComponent extends LightningElement {
-  @track value='hello my name is rishabh'; //reactive in nature 
-  //public method
-  @api handleValueChange() {
-    this.value='nice to meet you';
-  }
-}
