f7d4334c83af032929baba35c6794d8767ff7124
rishsingh1999 <104893374+rishsingh1999@users.noreply.github.com>
2022-05-15 19:50:18 +0530

    Delete parentWebComponent.js

--- video 3 assignment 2/parentWebComponent.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement } from "lwc";
-
-export default class ParentWebComponent extends LightningElement {
-  handleClick() {
-    //firing an child method
-    this.template.querySelector("c-child-web-component").handleValueChange();
-  }
-}
