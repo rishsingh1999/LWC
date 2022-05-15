7532f45b57b012b52a8650e926e8b1666e3a5ad7
rishsingh1999 <104893374+rishsingh1999@users.noreply.github.com>
2022-05-15 19:51:16 +0530

    Add files via upload

---
+++ video 3 assignment 2/parentWebComponent/parentWebComponent.js
@@ -0,0 +1,9 @@
+import { LightningElement } from "lwc";
+
+export default class ParentWebComponent extends LightningElement {
+  handleClick() {
+    //firing an child method
+    this.template.querySelector("c-child-web-component").handleValueChange();
+  }
+  
+}
