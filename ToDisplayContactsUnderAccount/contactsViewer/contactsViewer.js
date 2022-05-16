40e4d5c3fce3d336bb1f9ee628c913a861a1e2fd
rishsingh1999 <104893374+rishsingh1999@users.noreply.github.com>
2022-05-16 19:03:41 +0530

    Update contactsViewer.js

--- ToDisplayContactsUnderAccount/contactsViewer/contactsViewer.js
+++ ToDisplayContactsUnderAccount/contactsViewer/contactsViewer.js
@@ -1,12 +1,11 @@
 import { api, LightningElement,track, wire} from 'lwc';
-import getContactsRelatedToAccount from '@salesforce/apex/ContactFinder.getContactsRelatedToAccount';
 import findContacts from '@salesforce/apex/ContactFinder.findContacts';
-const DELAY = 300;
+const DELAY = 150;
 export default class ContactsViewer extends LightningElement {
     @api recordId;
     @track contacts;
-  //  @wire(getContactsRelatedToAccount, {accId: '$recordId'}) contacts;
-    @api searchKey;
+    @api flexipageRegionWidth
+    @track searchKey;
     searchKey = '';
 
     @wire(findContacts, { searchKey: '$searchKey',accId: '$recordId' })
