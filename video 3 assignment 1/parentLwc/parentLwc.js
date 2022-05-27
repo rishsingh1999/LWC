abc026b6be8c9c7ce15096d35452ef8eb5f99077
rishsingh1999 <104893374+rishsingh1999@users.noreply.github.com>
2022-05-27 17:52:00 +0530

    Update parentLwc.js

--- video 3 assignment 1/parentLwc/parentLwc.js
+++ video 3 assignment 1/parentLwc/parentLwc.js
@@ -1,11 +1,7 @@
 import { LightningElement, track } from "lwc";
-
 export default class ParentLwc extends LightningElement {
-    @track progressValue = 0;
-    
-    hanldeProgressValueChange(event) {
-      
-        this.progressValue = event.detail;
-    
+    @track progressValue = 0;    
+    hanldeProgressValueChange(event) {      
+        this.progressValue = event.detail;    
   }
 }
